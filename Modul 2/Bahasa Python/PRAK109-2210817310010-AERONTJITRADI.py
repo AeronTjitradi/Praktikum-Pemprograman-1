59a794a229bf7111e19cf3b7aa93e50b3ebcd7c5
AeronTjitradi <115365108+AeronTjitradi@users.noreply.github.com>
2022-10-16 00:23:56 +0800

    Delete Modul 2 directory

--- Modul 2/Bahasa Python/PRAK109-2210817310010-AERONTJITRADI.py
+++
@@ -1,6 +0,0 @@
-a=958730
-b=5
-c=a/b
-print("Jumlah Pasukan Yang Dibawa Yu Zhong =",a)
-print("Jumlah Pahlawan =",b)
-print("Jumlah Pasukan Yang Harus Dikalahkan Setiap Pahlawan Adalah {} Pasukan".format(round(c)))
