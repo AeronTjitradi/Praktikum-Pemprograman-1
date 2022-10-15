f74c159e7473aedd5bffcd31e22e96d8fe92f47c
AeronTjitradi <115365108+AeronTjitradi@users.noreply.github.com>
2022-10-15 23:16:58 +0800

    Add files via upload

---
+++ Modul 2/Bahasa Python/PRAK104-2210817310010-AERONTJITRADI.py
@@ -0,0 +1,10 @@
+a=400000
+b=350000
+d1=13
+d2=21
+hasil1=a-d1/100*a
+hasil2=b-d2/100*b
+print("Harga Sepatu A Adalah", a)
+print("Harga Sepatu B Adalah", b)
+print("Sepatu A Mendapat Diskon 13% Sehingga Harganya Menjadi",round(hasil1))
+print("Sepatu B Mendapat Diskon 21% Sehingga Harganya Menjadi",round(hasil2))
